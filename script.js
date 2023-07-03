b07b706cf7649ba2ffd2b7992fc83de4b3142401
Mohamed Ibrahim Zaky <125222969+Mohamedzake@users.noreply.github.com>
2023-07-03 16:17:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+const modal = document.querySelector(".modal");
+const btnCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+const openModel = function () {
+  console.log("Button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModel = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", openModel);
+}
+btnCloseModal.addEventListener("click", closeModel);
+overlay.addEventListener("click", closeModel);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModel();
+  }
+});
